c42da25970a802abefc5263aa067c562a67709b0
Netsl <vanya6537@gmail.com>
2021-03-10 13:13:56 +0400

    refactor: removed unused code

--- config/webpack.common.config.js
+++ config/webpack.common.config.js
@@ -28,14 +28,6 @@ module.exports = {
                     }
                 ]
             }
-            // {
-            //     test: /\.s[ac]ss$/i,
-            //     use: [
-            //         "style-loader",
-            //         "css-loader",
-            //         "sass-loader"
-            //     ],
-            // }
         ]
     }
 };
