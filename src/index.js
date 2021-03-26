7e304dc80500d23d17f65edc9a9c0fa836ea215c
Netsl <vanya6537@gmail.com>
2021-03-26 03:08:10 +0300

    docs: added JSDOC to checkBrowser, displayMessage, checkAndDisplay

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 const supportedBrowsers = require('../supportedBrowsers.js');
 
 module.exports = {
+    /**
+     * Check if the current user agent matches the support list
+     * @returns {boolean}
+     */
     checkBrowser() {
         return supportedBrowsers.test(navigator.userAgent);
     },
+    /**
+     * Display message on the current page
+     */
     displayMessage(message) {
         document.open();
         document.write(message);
         document.close();
     },
+    /**
+     * Display message on the current page if needed
+     * @param {string} unsupportedMessage - The message to display
+     */
     checkAndDisplay(unsupportedMessage = '<h1>Browser is not supported!</h1>') {
         const isSupported = this.checkBrowser();
         if (!isSupported) this.displayMessage(unsupportedMessage);
